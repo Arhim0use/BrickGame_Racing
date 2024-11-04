91363f0c3e8bc146f93fd68eee036f723e7488a9
arhim0use <chingisbek@gmail.com>
2024-11-04 15:05:21 +0300

    Split managers, implement Basic Level Manager

--- Racing_v2/Racing_v2/BrickGame/Racing/RevorkedModel/LevelManager.swift
+++ Racing_v2/Racing_v2/BrickGame/Racing/RevorkedModel/LevelManager.swift
@@ -7,58 +7,37 @@
 
 import Foundation
 
-//protocol Scorable {
-//    func addPoints(_ points: Int)
-//    func addPoints()
-//    func resetScore()
-//}
-//
-//class ScoreManager {
-//    
-//}
-//
-//protocol LevelManager {
-//    var gameModel: GameModel? { get set }
-//    func levelUp()
-//}
-//
-//class LevelHandler: LevelManager {
-//    weak var gameModel: GameModel?
-//    
-//    deinit {
-//        print("🛑 levelmanager deinit ")
-//    }
-//    
-//    /// - Note: use this to update lvl and add score points
-//    func addPoints(_ points: RacingInt) {
-//        guard let model = gameModel, let model = model as? RacingModel else {
-//            return
-//        }
-//        
-//        for (pos, enemy) in model.enemys.enumerated() {
-//            if enemy.yPos >= RacingDefines.yBorderSize {
-//                model.enemys.remove(at: pos)
-//                model.score += Int32(points)
-//                if model.gameInfo.level < RacingDefines.maxLevel {
-//                    levelUp()
-//                }
-//            }
-//        }
-//    }
-//    
-//    /// - Note: use this to update lvl and add 1 score point
-//    func addPoints() {
-//        addPoints(1)
-//    }
-//    
-//    /// - Note: add lvl if gameInfo.score % RacingDefines.toNextLvl
-//    func levelUp() {
-//        guard let model = gameModel else {
-//            return
-//        }
-//        
-//        if RacingInt(model.gameInfo.score) % RacingDefines.toNextLvl == 0 {
-////            model.gameInfoWrapper.gameInfo.level +=  1
-//        }
-//    }
-//}   //  class LevelManager
+protocol LevelManager {
+    var gameModel: GameModel { get set }
+    func levelUp()
+}
+
+class LevelHandler: LevelManager {
+    var gameModel: GameModel
+    
+    deinit {
+        print("🛑 levelmanager deinit ")
+    }
+    
+    required init(gameModel: GameModel) {
+        self.gameModel = gameModel
+    }
+    
+    /// - Note: add lvl and speed up in gameInfo
+    func levelUp() {
+        guard gameModel.level <= RacingDefines.maxLevel else {
+            return
+        }
+        
+        changeLVL()
+    }
+    
+    func changeLVL() {
+        if let model = gameModel as? RacingModel,
+            RacingInt(gameModel.gameInfo.score) % RacingDefines.toNextLvl == 0
+        {
+            model.gameInfoWrapper.gameInfo.level +=  1
+            model.speed += 50
+        }
+    }
+}   //  class LevelManager
