55855b43978a863ded9fa756303a78b578aef720
arhim0use <chingisbek@gmail.com>
2024-11-06 18:36:50 +0300

    commit prev code

--- Racing_v2/Racing_v2/BrickGame/ViewModel/BricGameViewModel.swift
+++ Racing_v2/Racing_v2/BrickGame/ViewModel/BricGameViewModel.swift
@@ -26,78 +26,78 @@ protocol BricGameViewModel {
     func endGame()
 }
 
-class RacingViewModel: ObservableObject, BricGameViewModel {
-    /*private*/ var _model: PREV_GameModel
-    private var _gameInfo: GameInfoWrapper
-
-    init() {
-        self._gameInfo = GameInfoWrapper()
-        self._model = PREV_RacingModel(gameInfoWrapper: self._gameInfo)
-    }
-    
-    deinit {
-        print("🛑 RacingViewModel deinit ")
-    }
-    
-    var gameInfo: GameInfo_t {
-        return _gameInfo.gameInfo
-    }
-    
-    var currentState: BrickGameGameState {
-        guard let model = _model as? PREV_RacingModel else {
-            return .end
-        }
-        
-        return model.racingFSM.currentState
-    }
-
-    var score: RacingInt {
-        guard let model = self._model as? PREV_RacingModel else {
-            return 0
-        }
-        
-        return RacingInt(model.gameInfo.score)
-    }
-
-    var lives: RacingInt {
-        guard let model = self._model as? PREV_RacingModel else {
-            return 0
-        }
-        return RacingInt(model.livesCount)
-    }
-
-    var level: RacingInt {
-        guard let model = self._model as? PREV_RacingModel else {
-            return 0
-        }
-        return RacingInt(model.gameInfo.level)
-    }
-
-    func startGame() {
-        guard let model = self._model as? PREV_RacingModel else {
-            return
-        }
-        
-        model.startGame()
-    }
-
-    func pauseGame() {
-        guard let model = self._model as? PREV_RacingModel else {
-            return
-        }
-        
-        model.pauseGame()
-    }
-
-    func updateGame(with action: UserAction?) {
-        guard let model = self._model as? PREV_RacingModel else {
-            return
-        }
-        
-        model.racingFSM.gameLoop(action)
-    }
-
-    func endGame() {
-        // Сохранение счета, окончание игры
-    }
-}
+//class RacingViewModel: ObservableObject, BricGameViewModel {
+//    /*private*/ var _model: PREV_GameModel
+//    private var _gameInfo: GameInfoWrapper
+//
+//    init() {
+//        self._gameInfo = GameInfoWrapper()
+//        self._model = PREV_RacingModel(gameInfoWrapper: self._gameInfo)
+//    }
+//    
+//    deinit {
+//        print("🛑 RacingViewModel deinit ")
+//    }
+//    
+//    var gameInfo: GameInfo_t {
+//        return _gameInfo.gameInfo
+//    }
+//    
+//    var currentState: BrickGameGameState {
+//        guard let model = _model as? PREV_RacingModel else {
+//            return .end
+//        }
+//        
+//        return model.racingFSM.currentState
+//    }
+//
+//    var score: RacingInt {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return 0
+//        }
+//        
+//        return RacingInt(model.gameInfo.score)
+//    }
+//
+//    var lives: RacingInt {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return 0
+//        }
+//        return RacingInt(model.livesCount)
+//    }
+//
+//    var level: RacingInt {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return 0
+//        }
+//        return RacingInt(model.gameInfo.level)
+//    }
+//
+//    func startGame() {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return
+//        }
+//        
+//        model.startGame()
+//    }
+//
+//    func pauseGame() {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return
+//        }
+//        
+//        model.pauseGame()
+//    }
+//
+//    func updateGame(with action: UserAction?) {
+//        guard let model = self._model as? PREV_RacingModel else {
+//            return
+//        }
+//        
+//        model.racingFSM.gameLoop(action)
+//    }
+//
+//    func endGame() {
+//        // Сохранение счета, окончание игры
+//    }
+//}
